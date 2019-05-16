41e5ff796edea96132105e086c56ec2c39f32ce8
Randy Guo <1155074290@link.cuhk.edu.hk>
2019-05-16 21:50:43 +0800

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -51,43 +51,29 @@ def GetSaveXML():
 
 return
 
+==========================================================
+
+from flask import Flask, request,make_response
+
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 
+def create_app(config=None):
+    app = Flask(__name__)
+    #app.config.from_object(config)
+    if config is not None:
+        app.config.from_pyfile(config)
+    # send CORS headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        if request.method == 'OPTIONS':
+            response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+            headers = request.headers.get('Access-Control-Request-Headers')
+            if headers:
+                response.headers['Access-Control-Allow-Headers'] = headers
+                ##response.headers['Authorization'] = 'xiaominggessdfs3432ds34ds32432cedsad332e23'
+        return response
 
-## Welcome to GitHub Pages
-
-You can use the [editor on GitHub](https://github.com/Randyedu/randyguo.github.io/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
-
-Whenever you commit to this repository, GitHub Pages will run [Jekyll](https://jekyllrb.com/) to rebuild the pages in your site, from the content in your Markdown files.
-
-### Markdown
-
-Markdown is a lightweight and easy-to-use syntax for styling your writing. It includes conventions for
-
-```markdown
-Syntax highlighted code block
-
-# Header 1
-## Header 2
-### Header 3
-
-- Bulleted
-- List
-
-1. Numbered
-2. List
-
-**Bold** and _Italic_ and `Code` text
-
-[Link](url) and ![Image](src)
-```
-
-For more details see [GitHub Flavored Markdown](https://guides.github.com/features/mastering-markdown/).
-
-### Jekyll Themes
-
-Your Pages site will use the layout and styles from the Jekyll theme you have selected in your [repository settings](https://github.com/Randyedu/randyguo.github.io/settings). The name of this theme is saved in the Jekyll `_config.yml` configuration file.
-
-### Support or Contact
-
-Having trouble with Pages? Check out our [documentation](https://help.github.com/categories/github-pages-basics/) or [contact support](https://github.com/contact) and we’ll help you sort it out.
